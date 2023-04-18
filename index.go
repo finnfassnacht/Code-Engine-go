7da3bdb1b368be4d906daa7ff5cbf9807c0f360f
finn <finnfassnacht@gmail.com>
2023-04-18 10:45:03 +0200

    initial commit

---
+++ index.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloworld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, World!")
+}
+
+func main() {
+	http.HandleFunc("/", helloworld)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
